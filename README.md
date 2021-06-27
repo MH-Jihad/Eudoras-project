1380d1d336d9386f5042ed32e206e848ef9cb3f7
MH-Jihad <imjihad29@gmail.com>
2021-06-27 14:50:41 +0600

    eudora web template

---
+++ README.md
@@ -0,0 +1,1 @@
+# Eudoras-project
---
+++ assets/js/main.js
@@ -0,0 +1,220 @@
+// init Isotope
+var $grid = $('.isotope-item').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.item-menu').on( 'click', 'li', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+
+ //navbar
+
+ //////////////mmenu
+
+// document.addEventListener(
+//     "DOMContentLoaded", () => {
+//         new Mmenu( "#justmenu" );
+//     }
+// );
+
+
+ /////////////mmenu
+
+function myFunction(){
+  var x = document.getElementById("menu");
+
+  if(x.style.display == "block"){
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
+
+/////////isotope product details
+
+
+//////////isotope hover
+
+
+
+
+
+/////////////////////////////////////////isotope product details////////////////////////////////////
+function myclass(){
+  var x = document.getElementById("showit");
+
+  if(x.style.display == "block"){
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
+////two
+function myclassone(){
+  var x = document.getElementById("showitone");
+
+  if(x.style.display == "block"){
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
+
+//three
+function myclasstwo(){
+  var x = document.getElementById("showittwo");
+
+  if(x.style.display == "block"){
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
+///four
+
+
+function myclassthree(){
+  var x = document.getElementById("showitthree");
+
+  if(x.style.display == "block"){
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
+///five
+
+function myclassfour(){
+  var x = document.getElementById("showitfour");
+
+  if(x.style.display == "block"){
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
+//six
+
+function myclassfive(){
+  var x = document.getElementById("showitfive");
+
+  if(x.style.display == "block"){
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
+
+//seven
+
+
+function myclasssix(){
+  var x = document.getElementById("showitsix");
+
+  if(x.style.display == "block"){
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
+///eight
+
+function myclassseven(){
+  var x = document.getElementById("showitseven");
+
+  if(x.style.display == "block"){
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
+///nine
+
+function myclasseight(){
+  var x = document.getElementById("showiteight");
+
+  if(x.style.display == "block"){
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
+///ten
+
+function myclassnine(){
+  var x = document.getElementById("showitnine");
+
+  if(x.style.display == "block"){
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
+///eleven
+
+function myclassten(){
+  var x = document.getElementById("showitten");
+
+  if(x.style.display == "block"){
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
+//twelve
+
+function myclasseleven(){
+  var x = document.getElementById("showiteleven");
+
+  if(x.style.display == "block"){
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
+//thirtin//
+
+function myclasstwelve(){
+  var x = document.getElementById("showittwelve");
+
+  if(x.style.display == "block"){
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
+//fourtin
+function myclassthirtin(){
+  var x = document.getElementById("showitthirtin");
+  if(x.style.display == "block"){
+    x.style.display = "none";
+  }
+  else{
+    x.style.display = "block";
+  }
+}
+///////lear more hide
+// function myclassthirtin(){
+//   var x = document.getElementById("lrnbtn");
+//   if(x.style.display == "block"){
+//     x.style.display = "none";
+//   }
+//   else{
+//     x.style.display = "block";
+//   }
+// }
